94e93081c3b594338f13da2f23226d91aaac9b7b
Huy Luong <huyluong@Huys-MacBook-Pro.local>
2017-01-30 15:29:07 -0500

    sign in and go to individual page

--- src/DoerProfile.js
+++ src/DoerProfile.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import Dialog from 'material-ui/Dialog';
-
-class DoerDialog extends Component {
-
+import base from './config';
+class DoerProfile extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: []
+    }
+  }
+  componentDidMount () {
+  base.fetch(`/doers/${this.props.params.name}`, {
+    context: this,
+    asArray: true,
+    then: (data) => {
+      this.setState({
+        name: data
+      })
+      console.log("what is", data)
+    }
+  })
+}
 render (){
   return (
-    <Dialog
-        title="Scrollable Dialog"
-        open={this.state.open}
-        autoScrollBodyContent={true}
-      >
-      <h1>Doer</h1>
-      <h3> Name </h3>
-      {this.state.name}
-
-      <h3>About</h3>
-      {this.state.about}
-
-      <h3> Contact Information </h3>
-      {this.state.email}
-      </Dialog>
+    <div>
+    {this.state.name.map (first => {
+      return (<p>{first.firstName}</p>)
+    })}
+    </div>
   )
   }
 }
-
-export default DoerDialog
+export default DoerProfile
