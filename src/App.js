c9d50b3bed38730ae253a2e69cc81cb274e8ddee
Huy Luong <huyluong@huys-mbp.attlocal.net>
2017-02-10 00:27:14 -0500

    responsive phase 1

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Doers from './Doers'
 import Donors from './Donors'
 import AppBar from 'material-ui/AppBar';
 import doerLogin from './doerLogin';
+import FlatButton from 'material-ui/FlatButton';
 
 const style = {
   margin: 12,
@@ -42,7 +43,7 @@ class App extends Component {
           <div className="App">
               <header className="App-header">
                 <div className="logo">
-                  <img src= { pic }/>
+                  <img className='logoPic' src= { pic }/>
                 </div>
                 <div className='link'>
                   <Link className='link' to="/doerLogin">Doer login</Link>
@@ -54,14 +55,14 @@ class App extends Component {
                   <RaisedButton label="Doers" backgroundColor='#d15e29' style={style} className="buttons"
                     onClick={this.handleToggle}/>
 
-                  <Drawer open={this.state.open}>
+                  <Drawer open={this.state.open} containerClassName='testDraw'>
                       <AppBar showMenuIconButton={false} title="Doers" style={style2}/>
                       <Doers/>
                   </Drawer>
 
                   <RaisedButton label="Donors" backgroundColor='#d15e29' style={style} className="buttons"
                    onClick={this.handleToggle2}/>
-                  <Drawer width={250} openSecondary={true} open={this.state.open2} >
+                  <Drawer width={250} openSecondary={true} open={this.state.open2} containerClassName='testDraw' >
                       <AppBar showMenuIconButton={false} title="Donors" style={style2}/>
                       <Donors />
                   </Drawer>
